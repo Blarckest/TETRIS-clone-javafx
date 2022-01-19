8a76890f056555dc1812508966b49d12ec6460bf
yogourves <yoann.gourves@gmail.com>
2022-01-19 18:38:38 -0500

    update javadoc (manquait une fonction de commenté)

--- Code/src/model/utils/looper/IObserver.java
+++ Code/src/model/utils/looper/IObserver.java
@@ -4,5 +4,8 @@ package model.utils.looper;
  * interface definissant les fonctionnalités d'un observateur
  */
 public interface IObserver {
+    /**
+     * action a faire lors d'une notification d'un sujet
+     */
     void doAction();
 }
